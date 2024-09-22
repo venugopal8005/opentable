13cd843f97d4a4ad5dcc8ba6788f08e24f4fd485
venugopal <119839956+venugopal8005@users.noreply.github.com>
2024-09-22 12:40:28 +0530

    simple navigation

--- OPENTABLE/src/App.jsx
+++ OPENTABLE/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import './App.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Aboutus from './components/Aboutus'
+import Login from './components/Login'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element = {<Navbar/>}> 
+                  <Route index element = {<Home/>}></Route>
+                  <Route path='aboutus' element = {<Aboutus/>}></Route>
+                  <Route path='getstarted' element = {<Login/>}></Route>
+              </Route>
 
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- OPENTABLE/src/components/Navbar.jsx
+++ OPENTABLE/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react"
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
-    <div>
-      
-    </div>
+   <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/aboutus">About us</Link></li>
+        <li><Link to="/getstarted">Login</Link></li>
+      </ul>
+    </nav>
+    <Outlet/>
+    </>
   )
 };
 
