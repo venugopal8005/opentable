eb7752385ea9bb7d969ecd7c8a55efb5a0f8f56b
venugopal <119839956+venugopal8005@users.noreply.github.com>
2025-02-08 13:56:52 +0530

    idk

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb+srv://vgv38634:oY8ktVmskcKRYkbE@okayletsdoit.poy0m.mongodb.net/";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+
+async function run() {
+  try {
+   
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
